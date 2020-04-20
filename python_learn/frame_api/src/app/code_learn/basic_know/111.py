eaa735ec41f87a7c49f7b48012205ddcf66b7071
qingxiaoye <1427939993@qq.com>
2020-04-20 10:38:08 +0800

    提交新的代码

--- python_learn/frame_api/src/app/code_learn/basic_know/111.py
+++ python_learn/frame_api/src/app/code_learn/basic_know/111.py
@@ -1,4 +1,8 @@
-str1 = 'I want to go home'
-list1 = str1.split(' ')
-print(list1.append('xq'))
-print(list1)
+class A(object):
+    bar = 1
+
+a = A()
+print(getattr(a, 'bar') )       # 获取属性 bar 值
+
+
+print(getattr(a, 'bar2', 3))    # 属性 bar2 不存在，但设置了默认值
